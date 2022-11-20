bc6a940c8f5cc2dad0c7ced6cd055cb7d0110f5f
anishazahan <anishazahan13@gmail.com>
2022-11-20 20:34:39 +0600

    listing quize instructions

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Main = () => {
   return (
-    <div>Main</div>
+    <div className='container'>
+      <h1 className='title text-light'>Quiz Application</h1>
+      <ol>
+        <li>You will be asked 10 questions one after another.</li>
+        <li>10 points are award for the correct answer.</li>
+        <li>Each questions has three options.You can chose only one.</li>
+        <li>You can review and Change answer before the quiz finish.</li>
+        <li>The result will be declared at the end of the quiz.</li>
+      </ol>
+    </div>
   )
 }
 
