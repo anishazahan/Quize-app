79238eb42b5dcaa344b1466fe5cca1f5bbd05661
anishazahan <anishazahan13@gmail.com>
2022-11-20 20:15:31 +0600

    make router path

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,22 @@
 
 import '../styles/App.css';
+import {createBrowserRouter,RouterProvider} from 'react-router-dom'
+
+////////------------react routes-----------------
+  const router = createBrowserRouter([
+    {
+      path :'/',
+      element : <div>Root element</div>
+    },
+    {
+      path :'/quiz',
+      element : <div>quize element</div>
+    },
+    {
+      path :'/result',
+      element : <div>result element</div>
+    }
+  ])
 
 function App() {
   return (
