839210fa183067d8b28febe0da2f08122dd0cb1d
anishazahan <anishazahan13@gmail.com>
2022-11-21 10:43:39 +0600

    onclick function add in next prev btns

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -2,14 +2,21 @@ import React from 'react'
 import './main.css'
 
 const Quiz = () => {
+  function onNext (){
+    console.log("next")
+  }
+  function onPrev (){
+    console.log("next")
+  }
+
   return (
     <div className='container'>
           <h1 className='title text-light'>Quiz Application</h1>
 
           {/* ----------------display question------------------ */}
           <div className="grid">
-            <button className='btn prev'>Prev</button>
-            <button className='btn next'>Next</button>
+            <button onClick={onPrev} className='btn prev'>Prev</button>
+            <button onClick={onNext} className='btn next'>Next</button>
           </div>
 
     </div>
