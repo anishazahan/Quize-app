880431f0d08b6ddeb0d5aa341843c0802ef09abf
anishazahan <anishazahan13@gmail.com>
2022-11-21 10:40:29 +0600

    prev and next btn styling

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import './main.css'
 
 const Quiz = () => {
   return (
-    <div>
-        
+    <div className='container'>
+          <h1 className='title text-light'>Quiz Application</h1>
+
+          {/* ----------------display question------------------ */}
+          <div className="grid">
+            <button className='btn prev'>Prev</button>
+            <button className='btn next'>Next</button>
+          </div>
+
     </div>
   )
 }
