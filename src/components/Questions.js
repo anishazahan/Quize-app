c4d1100f9db928f53f459a7fade8af689e69ff94
anishazahan <anishazahan13@gmail.com>
2022-11-21 20:52:57 +0600

    inputs radion btn condition for questions options

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Questions = () => {
+
+    const [checked,setChecked] = useState(undefined)
+    function onselect(){
+        setChecked(true)
+        console.log("radio btn change");
+    }
   return (
-    <div>Questions</div>
+    <div className='questions'>
+        <h2 className='questions__heading'>Simple question 1</h2>
+        <ul>
+            <li>
+                <input 
+                type="radio"
+                value={checked}
+                name="options"
+                id='q1-option'
+                onChange={onselect}
+                
+                />
+            </li>
+        </ul>
+
+    </div>
   )
 }
 
--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './main.css'
+import Questions from './Questions'
 
 const Quiz = () => {
   function onNext (){
@@ -13,6 +14,7 @@ const Quiz = () => {
     <div className='container'>
           <h1 className='title text-light'>Quiz Application</h1>
 
+          <Questions></Questions>
           {/* ----------------display question------------------ */}
           <div className="grid">
             <button onClick={onPrev} className='btn prev'>Prev</button>
