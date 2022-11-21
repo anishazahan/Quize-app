b6f31caeecbb25b4a1937ca32e9adb99e486b267
anishazahan <anishazahan13@gmail.com>
2022-11-22 00:16:20 +0600

    listing result components

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,8 +1,37 @@
 import React from 'react'
+import './main.css'
 
 const Result = () => {
   return (
-    <div>Result</div>
+    <div className='container'>
+       <h1 className='title text-light'>Quiz Result</h1>
+    <div className="result flex-center">
+    <div className="flex">
+      <span>Username</span>
+      <span className='bold'>Daily tution</span>
+    </div>
+    <div className="flex">
+      <span>Total Quiz Point :</span>
+      <span className='bold'>50</span>
+    </div>
+    <div className="flex">
+      <span>Total Questions :</span>
+      <span className='bold'>05</span>
+    </div>
+    <div className="flex">
+      <span>Total Attempts :</span>
+      <span className='bold'>03</span>
+    </div>
+    <div className="flex">
+      <span>Total Earn points :</span>
+      <span className='bold'>30</span>
+    </div>
+    <div className="flex">
+      <span>Quiz Result :</span>
+      <span className='bold'>Passed</span>
+    </div>
+    </div>
+    </div>
   )
 }
 
