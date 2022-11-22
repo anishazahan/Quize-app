eaa20e3e6a357ab4a9ddb01b40125b683ec48e79
anishazahan <anishazahan13@gmail.com>
2022-11-22 11:16:28 +0600

    question reducers actions method implement

--- src/Redux/question_reducer.js
+++ src/Redux/question_reducer.js
@@ -1,3 +1,23 @@
-export  const questionReducer = ()=>{
+import { createSlice } from "@reduxjs/toolkit"
 
-}
+// -----------create reducer with initial value---------------
+
+export  const questionReducer = createSlice({
+    name : "questions",
+    initialState : {
+        queue :[],
+        answer :[],
+        trace :0
+    },
+    reducers :{
+        startExamAction :(state , action)=>{
+     return{
+            ...state,
+            queue : action.payload
+        }
+        }
+    }
+})
+
+export const {startExamAction} = questionReducer.actions;
+export default questionReducer.reducer
--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,7 +1,8 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 
 // .............import reducer................
-import { questionReducer } from "./question_reducer";
+
+import  questionReducer  from "./question_reducer";
 import { resultReducer } from "./resultReducer";
 
 const rootReducer = combineReducers(
@@ -10,3 +11,6 @@ const rootReducer = combineReducers(
         result : resultReducer
     }
 )
+
+// .............create store with reducer................
+export default  configureStore({reducer : rootReducer});
