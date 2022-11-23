574d766c064aeb16f9c9914a1ff73cdaaa20c701
anishazahan <anishazahan13@gmail.com>
2022-11-23 21:06:02 +0600

    get server data

--- src/helper/Helper.js
+++ src/helper/Helper.js
@@ -1,6 +1,7 @@
 import { current } from "@reduxjs/toolkit";
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
+import axios from "axios";
 
 export function attemps_number (result){
     return result.filter(r=>r!== undefined).length;
@@ -22,3 +23,14 @@ export function CheckUserExist({children}){
     const auth = useSelector(state=>state.result.userId)
     return auth ? children : <Navigate to='/' replace={true}></Navigate>
 }
+
+///-----get server data---------
+
+export async function getServerData(url,callback){
+  const data = await (await axios.get(url))?.data;
+ return callback ? callback(data) : data;
+}
+
+///-----post server data---------
+
+getServerData('http://localhost:5000/api/result')
